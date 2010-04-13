71cee4da79c738e11e3edcbba4f61003ece5c7f5
U-thero-PC\thero <thero@thero-PC.(none)>
2010-04-13 13:33:36 -0400

    Monitor user by loop

--- src/com/tah/im/IMNotifier_b.java
+++ src/com/tah/im/IMNotifier_b.java
@@ -7,14 +7,15 @@ import improject.MessageListener;
 import improject.IMSession.IMService;
 
 
-public class IMNotifier {
+public class IMNotifier_b{
 
 	private IMSession session;
 	private String MainAccount;
 	private String MainPasswd;
+	private boolean[] UserStatus;  //note that big U
 	
 	//Constructor: login when creating the IMInterface
-	public IMNotifier(){
+	public IMNotifier_b(){
 		//login by this account		
 		this.MainAccount = "testIM5566@gmail.com";
 		this.MainPasswd = "hu183183";
@@ -77,6 +78,60 @@ public class IMNotifier {
         }); 
 		thread.start();		
 	}
+	
+	//Constructor with friend_list
+	public IMNotifier_b(final String[][] Data_list){
+		//login by this account		
+		this.MainAccount = "testIM5566@gmail.com";
+		this.MainPasswd = "hu183183";
+		
+		this.session = new IMSession();
+		
+		session.addLogin(IMService.GOOGLE, MainAccount, MainPasswd);
+		
+		//connect 
+		try { 
+			session.connect();
+		} catch (IMException e) {
+			e.printStackTrace();
+		}			
+		
+		this.UserStatus = getUserStatus(Data_list);		
+		
+		// Create Thread to keep the Main Account Online
+		// incoming Message will trigger Listener
+		Thread thread = new Thread(new Runnable() {
+			public void run() {
+				boolean[] tempStatus;
+                int[] S_table = new int[Data_list[0].length];
+				while(true) { 
+                	 try {
+         				Thread.sleep(5000);
+         				tempStatus = getUserStatus(Data_list);
+         				S_table = CompareStatus(tempStatus, UserStatus);
+         				for(int i=0;i<S_table.length;i++){
+         					switch(S_table[i]){
+         					case 1:
+         						System.out.println("User "+Data_list[0][i]+" is off-line.");
+         						break;
+         					case -1:
+         						System.out.println("User "+Data_list[0][i]+" is now online.");
+         						break;
+         					default:
+         						;
+         					}
+         				}
+         				UserStatus=getUserStatus(Data_list);
+         			} catch (InterruptedException e) {
+         				e.printStackTrace();
+         			}
+                } 
+            }
+            
+            
+        }); 
+		thread.start();		
+	}
 
 	public void Broadcast(final String[][] Data_list) {
 		
@@ -109,4 +164,45 @@ public class IMNotifier {
 		}				
 			
 	}//end of Broadcast
+	
+	public boolean[] getUserStatus(final String[][] Data_list){
+		boolean[] userStatus = new boolean[Data_list[0].length];  // note that small u
+		
+		try { 
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+				
+		for(int i = 0; i < Data_list[0].length; i++){
+			try {
+				userStatus[i] = session.isOnline(this.MainAccount, Data_list[0][i]);
+			} catch (IMException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		/*for(boolean b:userStatus){
+			System.out.println(b);
+		}*/
+		
+		return userStatus; 
+	}
+	
+	public int[] CompareStatus(boolean[] b1, boolean[] b2){  //b1 is now, b2 is previous
+		int[] S_table = new int[b1.length];
+		
+		if(b1.length==b2.length){
+			for(int i=0;i<b1.length;i++){
+				if(b1[i]!=b2[i]){
+					if(b1[i]==false) S_table[i]=1; //change to off-line
+					else S_table[i]=-1; //change to online
+				}
+				else{
+					S_table[i]=0; //no change
+				}
+			}
+		}
+		return S_table;
+	}
 }
--- src/com/tah/im/testIM.java
+++ src/com/tah/im/testIM.java
@@ -5,15 +5,17 @@ public class testIM {
 		String[][] data_list = {{"thero666@gmail.com", "talkabouthealth.com@gmail.com"}, 
                 {"http://yahoo.com", "http://www.google.com"}}; 
 		
-		String[][] data_list2 = {{"thero666@gmail.com", "talkabouthealth.com@gmail.com"}, 
+		/*String[][] data_list2 = {{"thero666@gmail.com", "talkabouthealth.com@gmail.com"}, 
                 {"http://www.facebook.com", "http://www.gamefaq.com"}};
-		
-		IMNotifier MyNotifier = new IMNotifier();
+		*/
+		IMNotifier MyNotifier = new IMNotifier(data_list);
 								
 		
-		MyNotifier.Broadcast(data_list);
+		//MyNotifier.Broadcast(data_list);
 		
-		MyNotifier.Broadcast(data_list2);
+		
+		
+		//MyNotifier.Broadcast(data_list2);
 	
 		
 	}
